5bea893fc020d0121b112540749b2263dd759777
Jonas Dixon <jonas.dixon.42@gmail.com>
2022-07-18 16:12:01 +0000

    Added functionality and driver function for program

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ import json
 import pandas as pd
 import sqlalchemy as db
 
-if __name__ == '__main__':
-  genre = str(input('Please input the media genre'))
-  
+def get_genre():
+  genre = str(input('Please input the media genre (Music, Movies, TV, or Sports)': ))
   if genre == 'Music':
-    
+    return genre
   elif genre == 'Movies':
-    
+    return genre
   elif genre == 'TV':
-  
+    return genre
   elif genre == 'Sports':
-    
+    return genre
   else:
-    print('Invalid genre entered.')
+    genre = str(input('Invalid genre entered. Please try again: '))
+
+def run_program():
+  genre = get_genre()
+
+if __name__ == '__main__':
+  run_program()
+  
+
