f8d84f4e4f349cdb82b1588446153b4c010bfd7a
kyle-olivier20 <98287478+kyle-olivier20@users.noreply.github.com>
2022-07-18 09:11:27 -0600

    Added input into main

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import requests
 import json
 import pandas as pd
 import sqlalchemy as db
+
+if __name__ == '__main__':
+  genre = str(input('Please input the media genre'))
+  
+  if genre == 'Music':
+    
+  elif genre == 'TV':
+  
+  elif genre == 'Sports':
+    
+  else:
+    print('Invalid genre entered.')
