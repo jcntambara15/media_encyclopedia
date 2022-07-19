815d00c7a776ba864c2ced447de231fcbbd55cc6
JC Ntambara <jc.ntambara@gmail.com>
2022-07-19 18:57:02 +0000

    got database for individual movie input working

--- Movie.py
+++ Movie.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pandas as pd
+import sqlalchemy as db	
 
 
 def movie_search(movie_input):
@@ -12,9 +14,33 @@ def movie_search(movie_input):
         except IndexError:
             print('No movies for this search')
 
+def movie_list(movie_input):
+        i = 0
+        movies = []
+        request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
+        try:
+            while i < len(request):
+                movies.append(request['results'][i]['original_title'])
+                i += 1
+            return movies
+        except IndexError:
+            return 'No movies for this search'
+
+def movie_database(movie_input):
+    """Adding the list of movies to a database"""
+    data = {"Names":movie_list(movie_input)}
+    my_data_frame = pd.DataFrame(data)
+    engine = db.create_engine('sqlite:///movies.db')
+    my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
+    col_names = ['Movie Title']
+    query_result = engine.execute("SELECT * FROM data;").fetchall()
+    return pd.DataFrame(query_result, columns = col_names)
+
 if __name__ == '__main__':
     movie_input = str(input('Search for movie or "no" to exit when prompted: '))
     while movie_input != ('no' or 'No'):
         movie_search(movie_input)
+        print(movie_list(movie_input))
+        print(movie_database(movie_input))
         print('Search for another movie?')
         movie_input = str(input())
