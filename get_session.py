d3276953f239d2a56c51294f7b25f51e67625b31
Jean Claude <72007802+jcntambara15@users.noreply.github.com>
2022-07-19 10:21:05 -0700

    testing if I can pull

--- get_session.py
+++ get_session.py
@@ -10,4 +10,5 @@ myKey = {"request_token": "6bc047b88f669d1fb86574f06381005d93d3517a"}
 
 new_session = requests.get('https://api.themoviedb.org/3/authentication/session/new?api_key=5cd843c18592532a3fdceb17ed37cb9d')
 print(new_session.json())
+#Checking
 
