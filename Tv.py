013c310cad7dee54a9ea8710de1f1c6c0835f128
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-19 17:29:01 +0000

    added exception handling

--- Tv.py
+++ Tv.py
@@ -2,19 +2,21 @@ import requests
 import pandas as pd
 import sqlalchemy as db	
 import json
-
-def tv_search(t):
+def tv_search(tv_input):
     i = 0
-    r = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(t)).json()
-    while i < len(r):
-        print(r['results'][i]['original_name'])
-        i += 1
+    request = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(tv_input)).json()
+    try:
+        while i < len(request):
+            print(request['results'][i]['original_name'])
+            i += 1
+    except IndexError:
+        print('No shows for this search')
         
 
 
 if __name__ == '__main__':
-    t = str(input('Search for TV Show or "no" to exit when prompted: '))
-    while t != ('no' or 'No'):
-        tv_search(t)
-        print('Would you like to search for another show?')
-        t = str(input())
+    tv_input = str(input('Search for TV Show or "no" to exit when prompted: '))
+    while tv_input != ('no' or 'No'):
+        tv_search(tv_input)
+        print('Search for another show?')
+        tv_input = str(input())
