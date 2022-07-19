8eaf70c954e87aad3fabaa72e2c017c95e9a5860
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-19 17:47:56 +0000

    Added release dates

--- Tv.py
+++ Tv.py
@@ -7,7 +7,7 @@ def tv_search(tv_input):
     request = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(tv_input)).json()
     try:
         while i < len(request):
-            print(request['results'][i]['original_name'])
+            print(request['results'][i]['original_name'], request['results'][i]['first_air_date'])
             i += 1
     except IndexError:
         print('No shows for this search')
@@ -15,16 +15,8 @@ def tv_search(tv_input):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     tv_input = str(input('Search for TV Show or "no" to exit when prompted: '))
     while tv_input != ('no' or 'No'):
         tv_search(tv_input)
         print('Search for another show?')
         tv_input = str(input())
-=======
-    t = str(input('Search for TV Show or "no" to exit when prompted: '))
-    while t != ('no' or 'No'):
-        tv_search(t)
-        print('Would you like to search for another show?')
-        t = str(input())
->>>>>>> a87195a0ad40fc7be026ae36f203af21ec76cc50
