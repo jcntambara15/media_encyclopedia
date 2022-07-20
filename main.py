7fe7c8e1bd4ee28a1f5b5ace2709557c9449dade
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-20 20:07:16 +0000

    changes

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import requests
 import json
 import pandas as pd
 import sqlalchemy as db
+import Sports_News
+import Spotify
 
 from Movie import movie_search()
 from Tv import tv_search()
 
+
 def get_genre():
   genre = str(input('Please input the media genre: (Music, Movies, TV, or Sports)'))
   if genre == 'Music':
-    return genre
+    Spotify.main()
   elif genre == 'Movies':
     movie_search()
   elif genre == 'TV':
     tv_search()
   elif genre == 'Sports':
-    return genre
+    Sports_News.main()
   else:
     print('Invalid genre entered. Please try again: ')
 
