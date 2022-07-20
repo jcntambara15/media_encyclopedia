7dc40f0ec300d48de8eec5195464bac96de6c6ed
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-20 16:53:39 +0000

    improved db

--- Movie.py
+++ Movie.py
@@ -3,18 +3,24 @@ import json
 import pandas as pd
 import sqlalchemy as db	
 
+col_names = ['Title', 'Release Date']
+df = pd.DataFrame(columns=col_names)
 
 def movie_search(movie_input):
         i = 0
         request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
         try:
             while i < len(request):
-                print(request['results'][i]['original_title'], request['results'][i]['release_date'])
+                '''print(request['results'][i]['original_title'], request['results'][i]['release_date'])'''
+                df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date']]
                 i += 1
         except IndexError:
             print('No movies for this search')
+        for i in range(len(df)):
+            print(df.loc[i, :].to_string())
 
-def movie_list(movie_input):
+
+'''def movie_list(movie_input):
         i = 0
         movies = []
         request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
@@ -51,14 +57,11 @@ def clear_data(movie_input):
     my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
     col_names = ['Movie Title', 'Release']
     query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)
+    return pd.DataFrame(query_result, columns = col_names)'''
 
 if __name__ == '__main__':
     movie_input = str(input('Search for movie or "no" to exit when prompted: '))
     while movie_input != ('no' or 'No'):
         movie_search(movie_input)
-        print(movie_list(movie_input))
-        print(movie_database(movie_input))
         print('Search for another movie?')
         movie_input = str(input())
-    clear_data(movie_input)
