b66477827e027bb542cebcdf8d182e7407c89fe6
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-20 15:50:50 +0000

    improved db?

--- Tv.py
+++ Tv.py
@@ -4,44 +4,47 @@ import sqlalchemy as db
 import json
 
 
+col_names = ['Title', 'Release Date']
+df = pd.DataFrame(columns=col_names)
+
 def tv_search(tv_input):
     i = 0
     request = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(tv_input)).json()
     try:
         while i < len(request):
-            print(request['results'][i]['original_name'], request['results'][i]['first_air_date'], request['results'][i]['id'])
+            '''print(request['results'][i]['original_name'], request['results'][i]['first_air_date'], request['results'][i]['id'])'''
+            df.loc[len(df.index)] = [request['results'][i]['original_name'], request['results'][i]['first_air_date']]
             i += 1
     except IndexError:
         print('No shows for this search')
+    for i in range(len(df)):
+         print(df.loc[i, :].to_string())
 
 
-def tv_list(movie_input):
+'''def tv_list(tv_input):
         i = 0
         tv = []
         request = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(tv_input)).json()
         try:
             while i < len(request):
-                tv.append(request['results'][i]['original_name'])
+                tv_data = [request['results'][i]['original_name'], request['results'][i]['first_air_date']]
+                tv.append(tv_data)
                 i += 1
             return tv
         except IndexError:
-            return 'No movies for this search'
+            return 'No movies for this search'''
 
-def movie_database(tv_input):
-    # Query sqlite database
-    # Store in dataframe
-    # Convert dataframe to dictionary https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_dict.html
-    # Pull list of movies from Names
-    # Convert added both lists to a set()
-    # Convert the set to a list
-    # Put that list into the dataframe like normal
-    # to_sql()
-    """Adding the list of movies to a database"""
+'''def tv_database(tv_input):
+    """Adding the list of shows to a database"""
+    show_list = []
+    for i in tv_list(tv_input):
+        show_list.append(i)
+    print(show_list)
     data = {"Names":tv_list(tv_input)}
     my_data_frame = pd.DataFrame(data)
     engine = db.create_engine('sqlite:///tv.db')
     my_data_frame.to_sql('data', con=engine, if_exists='append', index=False)
-    col_names = ['Show Title']
+    col_names = ['Show Title', 'Release Date']
     query_result = engine.execute("SELECT * FROM data;").fetchall()
     return pd.DataFrame(query_result, columns = col_names)
 
@@ -51,9 +54,9 @@ def clear_data(movie_input):
     my_data_frame = pd.DataFrame(data)
     engine = db.create_engine('sqlite:///tv.db')
     my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
-    col_names = ['Show Title']
+    col_names = ['Show Title', 'Release Date']
     query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)
+    return pd.DataFrame(query_result, columns = col_names)'''
 
 
 if __name__ == '__main__':
