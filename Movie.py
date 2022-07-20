9205a3c1927bcf7fba5eceeada4d13899920ba87
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-20 17:29:05 +0000

    Perfect db

--- Movie.py
+++ Movie.py
@@ -10,14 +10,11 @@ def movie_search(movie_input):
         i = 0
         request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
         try:
-            while i < len(request):
-                '''print(request['results'][i]['original_title'], request['results'][i]['release_date'])'''
+            while i < len(request['results']):
                 df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date']]
                 i += 1
         except IndexError:
             print('No movies for this search')
-        for i in range(len(df)):
-            print(df.loc[i, :].to_string())
 
 
 '''def movie_list(movie_input):
@@ -65,3 +62,5 @@ if __name__ == '__main__':
         movie_search(movie_input)
         print('Search for another movie?')
         movie_input = str(input())
+    for i in range(len(df)):
+        print(df.loc[i, :].to_string())
