0ef25290c72579014fd64fd9ffaf90ebb38fb8b2
kyle-olivier20 <98287478+kyle-olivier20@users.noreply.github.com>
2022-07-20 19:00:51 -0600

    New forms.py file

---
+++ Website/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    search_input = StringField('What genre of media would you like to see? (Music, Sports, Movies, TV Shows',
+                           validators=[DataRequired(), Length(min=2, max=10)])
+    submit = SubmitField('Enter')
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class SignInForm(FlaskForm):
+    pass
