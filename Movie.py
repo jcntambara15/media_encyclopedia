7bff1ae2625c197149896a67e16351825d1f3d70
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-21 01:04:14 +0000

    No changes

--- Movie.py
+++ Movie.py
@@ -17,45 +17,6 @@ def movie_search(movie_input):
             print('No movies for this search')
 
 
-'''def movie_list(movie_input):
-        i = 0
-        movies = []
-        request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
-        try:
-            while i < len(request):
-                movies.append(tuple((request['results'][i]['original_title'], request['results'][i]['release_date'])))
-                i += 1
-            return movies
-        except IndexError:
-            return 'No movies for this search'
-
-def movie_database(movie_input):
-    """Adding the list of movies to a database"""
-    m_list = []
-    releaz = []
-    for each_movie in movie_list(movie_input):
-        m_list.append(each_movie[0])
-        releaz.append(each_movie[1])
-    print(m_list)
-    print(releaz)
-    data = {"Names":m_list, "Release":releaz}
-    my_data_frame = pd.DataFrame(data)
-    engine = db.create_engine('sqlite:///movie.db')
-    my_data_frame.to_sql('data', con=engine, if_exists='append', index=False)
-    col_names = ['Movie Title', 'Release']
-    query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)
-
-def clear_data(movie_input):
-    """Clearing the data"""
-    data = {"Names": [], "Release": []}
-    my_data_frame = pd.DataFrame(data)
-    engine = db.create_engine('sqlite:///movies.db')
-    my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
-    col_names = ['Movie Title', 'Release']
-    query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)'''
-
 if __name__ == '__main__':
     movie_input = str(input('Search for movie or "no" to exit when prompted: '))
     while movie_input != ('no' or 'No'):
--- Tv.py
+++ Tv.py
@@ -18,44 +18,6 @@ def tv_search(tv_input):
         print('No shows for this search')
 
 
-'''def tv_list(tv_input):
-        i = 0
-        tv = []
-        request = requests.get('https://api.themoviedb.org/3/search/tv?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(tv_input)).json()
-        try:
-            while i < len(request):
-                tv_data = [request['results'][i]['original_name'], request['results'][i]['first_air_date']]
-                tv.append(tv_data)
-                i += 1
-            return tv
-        except IndexError:
-            return 'No movies for this search'''
-
-'''def tv_database(tv_input):
-    """Adding the list of shows to a database"""
-    show_list = []
-    for i in tv_list(tv_input):
-        show_list.append(i)
-    print(show_list)
-    data = {"Names":tv_list(tv_input)}
-    my_data_frame = pd.DataFrame(data)
-    engine = db.create_engine('sqlite:///tv.db')
-    my_data_frame.to_sql('data', con=engine, if_exists='append', index=False)
-    col_names = ['Show Title', 'Release Date']
-    query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)
-
-def clear_data(movie_input):
-    """Clearing the data"""
-    data = {"Names": []}
-    my_data_frame = pd.DataFrame(data)
-    engine = db.create_engine('sqlite:///tv.db')
-    my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
-    col_names = ['Show Title', 'Release Date']
-    query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result, columns = col_names)'''
-
-
 if __name__ == '__main__':
     tv_input = str(input('Search for TV Show or "no" to exit when prompted: '))
     while tv_input != ('no' or 'No'):
