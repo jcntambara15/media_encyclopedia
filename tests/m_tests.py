5f462d4bb21e21ac3cea8ca2497e96118fdb2704
kyle-olivier20 <98287478+kyle-olivier20@users.noreply.github.com>
2022-07-20 18:53:44 -0600

    New m_tests.py file

---
+++ tests/m_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import main
+
+
+'''class TestFileName(unittest.TestCase):
+    def test_function1(self):
+        self.assertEqual(0, 0)
+    def test_function2(self):
+        self.assertEqual(3, 3)
+        self.assertEqual(3.3, 3.3)'''
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
